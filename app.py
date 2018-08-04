ac778f93f06977553a25e251a98fe4ab56b4fc95
Adam Seiford <adamseiford@gmail.com>
2018-08-04 14:52:40 -0500

    scaffolded out four commands

--- app.py
+++ app.py
@@ -9,13 +9,14 @@ BOT_OAUTH = os.environ.get('BOT_OAUTH', None)
 app = Flask(__name__)
 slack_client = SlackClient(BOT_OAUTH)
 
+
 @app.route('/bot', methods=['POST'])
 def slack_post():
     slack_event = json.loads(request.data)
     if "challenge" in slack_event:
         return make_response(slack_event["challenge"], 200,
                     {"content_type": "application/json"
-                })
+        })
 
     user_check = slack_event['event'].get('user')
 
@@ -27,10 +28,38 @@ def slack_post():
         response_message = username + " in " + channel + " says: "
 
         res = slack_client.api_call("chat.postMessage", channel=channel,
-                              text=response_message)
+            text=response_message)
 
-        print res
     return Response(), 200
+
+
+@app.route('/lunch', methods=['POST'])
+def lunch_create():
+    # slack_event = json.loads(request)
+
+    text = request.form.get('text').split()
+    action = str(text[0]).upper()
+    response_message = ''
+
+    print action
+    print text[1]
+
+    channel = request.form.get('channel_id')
+
+    if action == 'START':
+        response_message = 'You want to start lunch at ' + text[1]
+    elif action == 'ADD':
+        response_message = 'You want to add '+text[1]+' to the list of options'
+    elif action == 'VOTE':
+        response_message = 'You want to vote on an option'
+    elif action == 'STATUS':
+        response_message = 'You want the status of a lunch'
+
+    res = slack_client.api_call("chat.postMessage", channel=channel,
+                                text=response_message)
+
+    return Response(), 200
+
 
 @app.route('/', methods=['GET'])
 def test():
