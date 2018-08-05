e25bcecdd7daed708799b55a54bb3bc290b3ae4c
Adam Seiford <adamseiford@gmail.com>
2018-08-04 23:04:44 -0500

    commenting all of the things

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 import os
-import random
 import json
+
+# hash things to ensure only valid requests from slack are processed
 import hashlib
 import hmac
+
+# all the typical flask things needed
 from flask import Flask, request, make_response, Response
+
+# wrapper around slack's API
 from slackclient import SlackClient
 
+# auth we use to tell slack who we are
+# from https://api.slack.com/apps/{{AppId}}/oauth?
+# Bot User OAuth Access Token
 BOT_OAUTH = os.environ.get("BOT_OAUTH", None)
+
+# secret that we use to validate requests
+# from https://api.slack.com/apps/{{AppId}}/general?
+# Signing Secret
 SIGNING_SECRET = os.environ.get("SIGNING_SECRET", None)
 slack_client = SlackClient(BOT_OAUTH)
 
@@ -15,38 +27,48 @@ app = Flask(__name__)
 @app.route("/lunch", methods=["POST"])
 def lunch_create():
 
+    # compare the signature of the request with one we calculate
+    # to ensure only requests from slack are processed
     if not valid_request(request.headers, request.get_data()):
         return Response(), 401
 
+    # turn the rest of the user's message into an array
+    # to process actions appropriately
     text = request.form.get("text").split()
     action = str(text[0]).upper()
+
     response_message = ""
 
+    # channel id where the command was run
     channel = request.form.get("channel_id")
+    # user id who ran the command
     user_id = request.form.get("user_id")
 
+    # start command
+    # user wants to start a new train
     if action == "START":
+
+        # make sure the user passed a lunch time argument
         if len(text) == 2:
             time = text[1]
+
+            # get the user's name
             user_name = request.form.get("user_name")
 
+            # todo: POST route to create a new game
+
+            # post a message back to the channel
             response_message = "@%s is starting a lunch train for %s" % (user_name, time)
             slack_client.api_call("chat.postMessage", channel=channel,
                 text=response_message, link_names=1)
 
-            remind_message = "/remind me to go to lunch at 8:00pm"
-
-            slack_client.api_call("chat.postEphemeral",
-                channel=channel,
-                text=remind_message,
-                link_names=1,
-                user=user_id)
-
         else:
+            # user did not pass in a lunch time
             response_message = "You did not pass in a time to go to lunch!"
             res = slack_client.api_call("chat.postEphemeral", channel=channel,
                 text=response_message, user=user_id)
 
+    # user wants to upvote a restaurant choice
     elif action == "YUM":
         # Add the message_ts to the user's order info
         message_action = request.form
@@ -54,25 +76,29 @@ def lunch_create():
         with open("./dialog-templates/vote.json") as json_data:
             dialog = json.load(json_data)
 
+        # overwriting some values
+        # since the upvote and downvote (yum and yuck)
+        # share the same dialog config
         dialog["submit_label"] = "Upvote!"
         dialog["callback_id"] = "dialog-vote-yum"
 
-        # Show the ordering dialog to the user
+        # Show the  dialog to the user
         open_dialog = slack_client.api_call(
             "dialog.open",
             trigger_id=message_action["trigger_id"],
             dialog=dialog
         )
 
-        # Update the message to show that we're in the process of taking their order
+        # Update the message to show that we're waiting for their answer
         slack_client.api_call(
             "chat.update",
             channel=["lunch"],
             ts="in progress message",
-            text=":pencil: Taking your order...",
+            text=":pencil: Awaiting your choice...",
             attachments=[]
         )
 
+    # user wants to downvote a restaurant choice
     elif action == "YUCK":
         # Add the message_ts to the user's order info
         message_action = request.form
@@ -80,30 +106,36 @@ def lunch_create():
         with open("./dialog-templates/vote.json") as json_data:
             dialog = json.load(json_data)
 
+        # overwriting some values
+        # since the upvote and downvote (yum and yuck)
+        # share the same dialog config
         dialog["submit_label"] = "Downvote!"
         dialog["callback_id"] = "dialog-vote-yuck"
 
-        # Show the ordering dialog to the user
+        # Show the dialog to the user
         open_dialog = slack_client.api_call(
             "dialog.open",
             trigger_id=message_action["trigger_id"],
             dialog=dialog
         )
 
-        # Update the message to show that we're in the process of taking their order
+        # Update the message to show that we're waiting for their answer
         slack_client.api_call(
             "chat.update",
             channel=["lunch"],
             ts="in progress message",
-            text=":pencil: Taking your order...",
+            text=":pencil: Awaiting your choice...",
             attachments=[]
         )
 
+    # user wants to add a restaurant choice
+    # to the train
     elif action == "ADD":
         response_message = "You want to add an option"
         res = slack_client.api_call("chat.postEphemeral", channel=channel,
             text=response_message, user=user_id)
 
+    # user wants to join a train
     elif action == "BOARD":
         # Add the message_ts to the user's order info
         message_action = request.form
@@ -111,14 +143,14 @@ def lunch_create():
         with open("./dialog-templates/board.json") as json_data:
             dialog = json.load(json_data)
 
-        # Show the ordering dialog to the user
+        # Show the dialog to the user
         open_dialog = slack_client.api_call(
             "dialog.open",
             trigger_id=message_action["trigger_id"],
             dialog=dialog
         )
 
-        # Update the message to show that we're in the process of taking their order
+        # Update the message to show that we're waiting for their answer
         slack_client.api_call(
             "chat.update",
             channel=["lunch"],
@@ -127,21 +159,25 @@ def lunch_create():
             attachments=[]
         )
 
+    # user wants the status of running trains
     elif action == "STATUS":
         response_message = "You want the status of a lunch"
         res = slack_client.api_call("chat.postEphemeral", channel=channel,
             text=response_message, user=user_id)
 
+    # first time config to associate a user to the app
     elif action == "REGISTER":
         response_message = "You want to register"
         res = slack_client.api_call("chat.postEphemeral", channel=channel,
             text=response_message, user=user_id)
 
+    # see how many votes a user has available
     elif action == "VOTES":
         response_message = "You have 12,345 votes remaining."
         res = slack_client.api_call("chat.postEphemeral", channel=channel,
             text=response_message, user=user_id)
 
+    # command printing out all available actions
     elif action == "HELP":
         response_message = "Available commands:\n"\
             "start: start a train\n"\
@@ -157,19 +193,23 @@ def lunch_create():
         res = slack_client.api_call("chat.postEphemeral", channel=channel,
             text=response_message, user=user_id)
 
+    # not listed command
+    # to print all users to the console
     elif action == "LIST":
 
         users = slack_client.api_call("users.list")["members"]
 
         # print users
 
-        wanted_keys = ['id', 'name', 'real_name'] # The keys you want
+        wanted_keys = ['id', 'name', 'real_name']
         for thing in users:
             print thing["name"]
             print thing["id"]
             print thing["real_name"]
             print ""
 
+    # catch all error message
+    # says "uh oh!" in binary!
     else:
         response_message = "01110101 01101000 00100000 "\
             "01101111 01101000 00100001\n"\
@@ -182,6 +222,8 @@ def lunch_create():
     return Response(), 200
 
 
+# callback for when a dialog is filled out
+# by a user
 @app.route("/dialog", methods=["POST"])
 def dialog_action():
     slack_event = json.loads(request.form["payload"])
@@ -190,8 +232,7 @@ def dialog_action():
     user_id = slack_event["user"].get("id")
     channel = slack_event["channel"].get("id")
 
-    print "user id: "+ user_id
-
+    # check if the dialog is to upvote/downvote an option
     if str(dialog_callback).startswith("dialog-vote"):
         restaurant_id = slack_event["submission"].get("restaurant_id")
         vote_count = slack_event["submission"].get("vote_count")
@@ -206,61 +247,56 @@ def dialog_action():
         else:
             message = "You have casted %s votes!" % vote_count
 
-
         print "restaurant choice: " + restaurant_id
         print "vote count:" + str(vote_count)
 
         res = slack_client.api_call("chat.postEphemeral", channel=channel,
             text=message, user=user_id)
 
+    # user has chosen a train to join
     elif str(dialog_callback).startswith("dialog-board-train"):
         train_id = slack_event["submission"].get("train_id")
 
-        print "train_id: " + train_id
-
     return Response(), 200
 
-
+# route to authenticate the bot for slack
 @app.route("/bot", methods=["POST"])
 def slack_post():
     slack_event = json.loads(request.data)
+
     if "challenge" in slack_event:
         return make_response(slack_event["challenge"], 200,
-                             {"content_type": "application/json"
-                              })
-
-    user_check = slack_event["event"].get("user")
-
-    # if request.form["token"] == SLACK_WEBHOOK_SECRET:
-    if user_check is not None:
-        print slack_event
-        channel = slack_event["event"]["channel"]
-        username = slack_event["event"]["user"]
-        response_message = username + " in " + channel + " says: "
-
-        res = slack_client.api_call("chat.postMessage", channel=channel,
-                                    text=response_message)
+            {"content_type": "application/json"
+        })
 
     return Response(), 200
 
-
+# empty route
 @app.route("/", methods=["GET"])
 def test():
     return Response("It works!")
 
 
+# validate the incoming request
+# has been sent from slack
 def valid_request(headers, body):
     timestamp = str(headers['X-Slack-Request-Timestamp'])
     version_number = "v0"
-    signature = str(headers['X-Slack-Signature'])
 
+    # signature provided by slack
+    request_signature = str(headers['X-Slack-Signature'])
+
+    # concat the challenge string
+    # https://api.slack.com/docs/verifying-requests-from-slack#a_recipe_for_security
     request_string = version_number + ":" + timestamp + ":" + str(body)
 
+    # compute our version of the signature
     server_signature = version_number + '=' + \
         hmac.new(SIGNING_SECRET, request_string, hashlib.sha256)\
         .hexdigest()
 
-    return signature == server_signature
+    # return the result of comparing the two signatures
+    return request_signature == server_signature
 
 
 if __name__ == "__main__":
